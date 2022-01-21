2a4894d0ede3db7e2b379faf3877536d709003f3
SkyBelll <whdals26444@gmail.com>
2022-01-22 01:28:11 +0900

    update sett

--- app/src/main/java/com/example/runtyou/LogFragment.kt
+++ app/src/main/java/com/example/runtyou/LogFragment.kt
@@ -1,7 +1,17 @@
 package com.example.runtyou
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class LogFragment : Fragment(){
-
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_log, container, false)
+    }
 }
