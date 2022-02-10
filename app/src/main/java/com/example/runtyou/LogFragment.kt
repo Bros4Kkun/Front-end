25e9e5f5a349592814d40b8de4a300f3ce29e292
SkyBelll <whdals26444@gmail.com>
2022-02-11 02:18:19 +0900

    Feat:현황 기능 추가

--- app/src/main/java/com/example/runtyou/LogFragment.kt
+++ app/src/main/java/com/example/runtyou/LogFragment.kt
@@ -1,15 +1,19 @@
 package com.example.runtyou
 
 
+
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.Button
 import androidx.fragment.app.Fragment
-import net.daum.mf.map.api.MapView
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.runtyou.databinding.FragmentLogBinding
 import kotlinx.android.synthetic.main.fragment_log.*
+import kotlinx.android.synthetic.main.list_child.*
+import net.daum.mf.map.api.MapView
 
 
 class LogFragment : Fragment(){
@@ -27,6 +31,21 @@ class LogFragment : Fragment(){
         val mapView = MapView(activity)
         map_view.addView(mapView)
 
+
+        recyclerview.layoutManager = LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false)
+        val data: MutableList<ExpandableListAdapter.Item> = ArrayList() // 데이터를 담을 List
+
+
+        data.add(ExpandableListAdapter.Item(ExpandableListAdapter.HEADER, "진행 중 심부름"))
+        data.add(ExpandableListAdapter.Item(ExpandableListAdapter.CHILD, "벌레 잡아주세요"))
+
+
+
+        data.add(ExpandableListAdapter.Item(ExpandableListAdapter.HEADER, "완료된 심부름"))
+        data.add(ExpandableListAdapter.Item(ExpandableListAdapter.CHILD, "편의점에서 우유 사주세요"))
+
+
+        recyclerview.adapter = ExpandableListAdapter(data)
 
     }
 }
